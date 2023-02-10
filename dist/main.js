a0f87920fffa6778dd053b5e3f633a1a16a8a897
Matheus Martinez <decretomusic@gmail.com>
2023-02-10 08:02:58 -0300

    feat: add eslint and html basic structure

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,7 @@
 "use strict";
-console.log('Hello world');
+const btnSearch = document.querySelector('#btn-search');
+const coinInput = document.querySelector('#coin-input');
+function handleSearch() {
+    console.log(coinInput.value);
+}
+btnSearch.addEventListener('click', handleSearch);
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello world');
+const btnSearch = <HTMLButtonElement>document.querySelector('#btn-search');
+const coinInput = <HTMLInputElement>document.querySelector('#coin-input');
+
+function handleSearch() {
+  console.log(coinInput.value);
+}
+
+btnSearch.addEventListener('click', handleSearch);
