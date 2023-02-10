e2621c0e7824b23079666f9bed7d5a930b8c136e
Matheus Martinez <decretomusic@gmail.com>
2023-02-10 08:10:49 -0300

    feat: gitignore

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const btnSearch = document.querySelector('#btn-search');
-const coinInput = document.querySelector('#coin-input');
-function handleSearch() {
-    console.log(coinInput.value);
-}
-btnSearch.addEventListener('click', handleSearch);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 const btnSearch = <HTMLButtonElement>document.querySelector('#btn-search');
 const coinInput = <HTMLInputElement>document.querySelector('#coin-input');
 
+
+
 function handleSearch() {
   console.log(coinInput.value);
 }
